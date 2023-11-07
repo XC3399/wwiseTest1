dc5a37cd1cdcb32dad7d90804f8dae3f9c2a278f
XC3399 <113364159+XC3399@users.noreply.github.com>
2023-11-07 12:21:02 +0000

    added interpolation and button script

--- Assets/button1Script.cs
+++ Assets/button1Script.cs
@@ -15,4 +15,44 @@ public class button1Script : MonoBehaviour
     {
         
     }
+
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("button0"))
+        {
+            AkSoundEngine.SetRTPCValue("musicProgression", 0);
+        }
+
+        else if (collider.CompareTag("button1"))
+        {
+            AkSoundEngine.SetRTPCValue("musicProgression", 20);
+        }
+
+        else if (collider.CompareTag("button2"))
+        {
+            AkSoundEngine.SetRTPCValue("musicProgression", 40);
+        }
+
+        else if (collider.CompareTag("button3"))
+        {
+            AkSoundEngine.SetRTPCValue("musicProgression", 60);
+        }
+
+        else if (collider.CompareTag("button4"))
+        {
+            AkSoundEngine.SetRTPCValue("musicProgression", 80);
+        }
+
+        else if (collider.CompareTag("button5"))
+        {
+            AkSoundEngine.SetRTPCValue("musicProgression", 100);
+        }
+    }
+
+  
+    
+
+
 }
+
