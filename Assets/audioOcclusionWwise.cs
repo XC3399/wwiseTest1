bfff44685573581e9b8e3ead265a2bbbdc37a2c6
XC3399 <113364159+XC3399@users.noreply.github.com>
2023-11-09 11:29:50 +0000

    added to occlusion and script

--- Assets/audioOcclusionWwise.cs
+++ Assets/audioOcclusionWwise.cs
@@ -17,7 +17,7 @@ public class audioOcclusionWwise : MonoBehaviour
     public string RTPC_Volume = "RTPC_Occlusion_Volume";
 
     public float loPassMax = 1;
-    public float volume = 1;
+    public float volumeMax = 1;
 
     public bool useDebug = false;
 
@@ -31,12 +31,44 @@ public class audioOcclusionWwise : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //set the raycast distance to sphere collider
+        maxDistanceOcclusion = GetComponent<SphereCollider>().radius;
+        AkSoundEngine.RegisterGameObj(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //do not occlude if out of occlusion range DEVIATION  
         
+
+        //do nothing if audio not defined or useOcclusion is false
+        if (!useOcclusion || audioListener == null) { return; }
+
+        //get direction from audio listener to this
+        Vector3 direction = audioListener.transform.position - this.transform.position;
+
+        //racast stored as vairable
+        RaycastHit outInfo;
+        //raycast
+        bool hit = Physics.Raycast(this.transform.position, direction, out outInfo, maxDistanceOcclusion);
+
+        //debug
+        if (hit)
+            if (useDebug) { print(outInfo.collider.gameObject.name); }
+
+        if (outInfo.collider.gameObject.name != nameOfListener && outInfo.collider.gameObject.name != ignoreTypeOccluder) 
+        {
+            //gameobject means that the value will be set per game object so can be used indavidualy per game object. 
+            AkSoundEngine.SetRTPCValue(RTPC_LoPass, loPassMax, gameObject);
+            AkSoundEngine.SetRTPCValue(RTPC_Volume, volumeMax, gameObject);
+        }
+        else
+        {
+            AkSoundEngine.SetRTPCValue(RTPC_LoPass, 0, gameObject);
+            AkSoundEngine.SetRTPCValue(RTPC_Volume, 0, gameObject);
+        }
+
+
     }
 }
